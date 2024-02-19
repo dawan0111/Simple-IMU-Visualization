c5d6239cdd7c2159d5387fd7ce4836a5c5d441e2
KDW <msd030428@gmail.com>
2024-02-19 17:03:59 +0900

    imu angular velocities to quaternion

--- include/imu_visualization/imu_visualization_node.hpp
+++ include/imu_visualization/imu_visualization_node.hpp
@@ -2,15 +2,26 @@
 #define IMU_VISUALIZATION_NODE_HPP_
 #include "imu_visualization/imu_util.hpp"
 #include <iostream>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/imu.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
 class ImuVisualizationNode : public rclcpp::Node {
 public:
   ImuVisualizationNode(const rclcpp::NodeOptions &options);
   void imuCallback(const sensor_msgs::msg::Imu::ConstSharedPtr msg);
 
 private:
+  void tfTransformTimerCallback();
+
+private:
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscriber_;
+  tf2::Quaternion angular_q_;
+  tf2::Quaternion imu_q_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+  rclcpp::TimerBase::SharedPtr tf_transform_timer_;
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
 #endif
--- src/imu_visualization_node.cpp
+++ src/imu_visualization_node.cpp
@@ -4,17 +4,35 @@ ImuVisualizationNode::ImuVisualizationNode(const rclcpp::NodeOptions &options)
     : Node(options.arguments()[0], options) {
   RCLCPP_INFO(this->get_logger(), "%s constructor",
               options.arguments()[0].c_str());
-
+  tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(*this);
+  angular_q_ = tf2::Quaternion(0, 0, 0, 1);
+  imu_q_ = tf2::Quaternion(0, 0, 0, 1);
   imu_subscriber_ = this->create_subscription<sensor_msgs::msg::Imu>(
       "imu", rclcpp::QoS(1),
       std::bind(&ImuVisualizationNode::imuCallback, this,
                 std::placeholders::_1));
+
+  tf_transform_timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(30),
+      std::bind(&ImuVisualizationNode::tfTransformTimerCallback, this));
 }
 
 void ImuVisualizationNode::imuCallback(
     const sensor_msgs::msg::Imu::ConstSharedPtr msg) {
   RCLCPP_INFO(this->get_logger(), "IMU Callback run!");
+  auto angular_velocities = msg->angular_velocity;
+  Eigen::Vector3d angular_vector;
+  angular_vector << angular_velocities.x, angular_velocities.y,
+      angular_velocities.z;
+  auto omega = ImuUtils::angularVel2Omega(angular_vector);
+  angular_q_ = ImuUtils::angularQuaternionUpdate(angular_q_, omega, 0.005);
+  imu_q_.setW(msg->orientation.w);
+  imu_q_.setX(msg->orientation.x);
+  imu_q_.setY(msg->orientation.y);
+  imu_q_.setZ(msg->orientation.z);
 }
+
+void ImuVisualizationNode::tfTransformTimerCallback() {}
 
 int32_t main(int argc, char **argv) {
   rclcpp::init(argc, argv);
